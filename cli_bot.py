2fed2a4117d365a3f17a91401f5ad854f057fa98
raichenstorm <raichenstorm@gmail.com>
2024-03-29 21:17:55 +0200

    1

---
+++ cli_bot.py
@@ -0,0 +1,34 @@
+from collections import UserDict
+
+class Field:
+    def __init__(self, value):
+        self.value = value
+
+    def __str__(self):
+        return str(self.value)
+
+class Name(Field):
+    # реалізація класу
+		pass
+
+class Phone(Field):
+    def __init__(self, value):
+        if len(value) == 10 and value.isdigit:
+            super().__init__(value)
+        else:
+            raise ValueError
+            
+
+class Record:
+    def __init__(self, name):
+        self.name = Name(name)
+        self.phones = []
+
+    # реалізація класу
+
+    def __str__(self):
+        return f"Contact name: {self.name.value}, phones: {'; '.join(p.value for p in self.phones)}"
+
+class AddressBook(UserDict):
+    # реалізація класу
+		pass
